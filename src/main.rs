ede6779f65adc4ecc240e632585dae2ebbf7f8bf
Itamar da Silva Soares <Itamar_Soares@zohomail.com>
2024-07-31 11:33:45 -0300

    get e post com Axum

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,51 @@
 use axum::{
-    routing::get,
-    Router
+    body::Body,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Router, Json,
 };
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
+    email: String,
+}
+
+async fn create_user() -> impl IntoResponse {
+    Response::builder()
+        .status(StatusCode::CREATED)
+        .body(Body::from("Usuário criado com sucesso"))
+        .unwrap()
+}
+
+async fn list_users() -> Json<Vec<User>> {
+    let users = vec![
+        User {
+            id: 1,
+            name: "Elijah".to_string(),
+            email: "elijah@example.com".to_string(),
+        },
+        User {
+            id: 2,
+            name: "John".to_string(),
+            email: "john@doe.com".to_string(),
+        },
+    ];
+    Json(users)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/create-user", post(create_user))
+        .route("/users", get(list_users))
+        ;
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Running on {}", listener.local_addr().unwrap());
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Running on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
