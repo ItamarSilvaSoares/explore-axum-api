e67c61647a2e0f606deca8abf56cbe42162e884a
Itamar da Silva Soares <Itamar_Soares@zohomail.com>
2024-08-02 09:44:39 -0300

    Adicionando um simples Middleware

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use axum::{body::Body, Extension, extract::{Path, Query}, http::StatusCode, Json, response::{IntoResponse, Response}, Router, routing::{delete, get, post}};
+use axum::{
+    body::Body,
+    Extension,
+    middleware::{self, Next},
+    extract::{Path, Query},
+    http::StatusCode,
+    Json,
+    response::{IntoResponse, Response},
+    Router,
+    routing::{delete, get, post}};
+use axum::http::Request;
 use serde::{Deserialize, Serialize};
 use sqlx::{Row, MySqlPool};
 use serde_json::json;
@@ -18,6 +28,12 @@ struct Page {
 #[derive(Deserialize)]
 struct Item {
     title: String
+}
+
+async fn logging_middleware(req: Request<Body>, next: Next) -> Response {
+    println!("Recebendo uma requisição para {}", req.uri());
+    
+    next.run(req).await
 }
 
 async fn delete_user(Path(user_id): Path<u64>) -> Result<Json<User>, impl IntoResponse> {
@@ -100,6 +116,7 @@ async fn main() {
         .route("/create-user", post(create_user))
         .route("/users", get(list_users))
         .layer(Extension(pool))
+        .layer(middleware::from_fn(logging_middleware))
         ;
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
